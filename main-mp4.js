403a345af23df8bf7b9e66733705f6010d33deeb
Anshul <anshulkhare7@gmail.com>
2021-12-15 15:39:47 +0530

    mp4 with videojs

---
+++ main-mp4.js
@@ -0,0 +1,12 @@
+var options = {
+    width: 500, 
+    height: 640, 
+    controls: true,        
+    poster: 'https://vz-4df53096-bb3.b-cdn.net/d67eb94c-8ca5-45ec-8947-172a46bc5274/thumbnail.jpg',    
+};
+
+var player = videojs('ap-video', options);
+
+player.src({   
+    src: 'https://aptest.b-cdn.net/shorts/vid-01.mp4'    
+});
